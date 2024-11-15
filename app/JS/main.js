548af8843633d692d30dfaefa60b0cf3579c95c0
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-11-15 12:31:01 -0500

    fgdfgdsfgsd

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,31 +1,33 @@
 import "../CSS/style.css";
 
-async function getData(/* could use paramater url or hard code it */) {
-    //try catch method - similar to if else
-    //an example of a gaurd clause - won't break the system
+async function getFilteredData(character) {
+    const individualResponse = await fetch("https://genshin.jmp.blue/characters/" + character);
+}
+
+async function getData() {
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+        const response = await fetch("https://genshin.jmp.blue/characters/");
         if (response.status != 200) {       /* 200 = it works */
             throw new Error(response);
         } else {
             const data = await response.json();
-            console.log(data);
-            //how do you put it on the screen
-            document.querySelector("h1").textContent = data.name;           /* will print it on the screen - text content ==> the text for the object */
+            const container = document.querySelector(".container");
+            data.forEach((character) => {
+                container.insertAdjacentHTML("beforeend",
+                    `<div class="card" id="">
+                        <h1 class="name">${character}</h1>
+                        <h3 class="title"></h3>
+                        <img src="" alt="" class="picture">
+                    </div>
+                    `
+                )
+            });
+
         }
     } catch (error) {
         console.log(error);
-        alert("sorry could not find that pocket monster")
+        alert("sorry could not find that genshin category")
     }
-
-    //fetch returns a promise    /* put api as a string inside of the parathesis */
-    //await will wait for the code to run before moving on
-    // const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");      /* doesn't matter what you call it */
-    //the code above is not giving you the data
-    //if you spell it wrong it would break
-
-    //const data = await response.json();             /* turns it into a json data - becomes data */
-    //console.log(data);
 
 }
 
