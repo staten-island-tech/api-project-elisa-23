bdb966f377004ea4e1d08b7e53337d1cf055e448
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-11-23 18:17:22 -0500

    hkjhkjh

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,7 +1,7 @@
 import "../CSS/style.css";
 
 const DOMSelectors = {
-    container: document.querySelector(".container")
+    container: document.querySelector("#container")
 }
 
 async function getFilteredData(character, use) {
@@ -38,11 +38,21 @@ async function getData() {
                 const element = await getFilteredData(character, "element");        /* or else it will return the promise */
                 const title = await getFilteredData(character, "title");
                 DOMSelectors.container.insertAdjacentHTML("beforeend",
-                    `<div class="card" id="${element}" data-id="${character}">
-                        <h1 class="name">${character}</h1>
-                        <h3 class="title">${title}</h3>
-                        <img src="${`https://genshin.jmp.blue/characters/${character}/icon`}" alt="the icon of ${character}" class="picture">
-                        <button class="more" data-id="${character}-button">more</button>
+                    `<div 
+                        class="w-[27%] h-[3rem] bg-yellow-200 rounded-lg flex flex-col items-center justify-evenly mt-20 transition-all duration-700 hover:shadow-lg" 
+                        id="${element}" 
+                        data-id="${character}">
+                        <h1 class="text-xl font-bold">${character}</h1>
+                        <h3 class="text-gray-600 text-center">${title}</h3>
+                        <img 
+                            src="https://genshin.jmp.blue/characters/${character}/icon" 
+                            alt="The icon of ${character}" 
+                            class="w-[3rem] h-[3rem] rounded-full object-cover shadow-md">
+                        <button 
+                            class="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600" 
+                            data-id="${character}-button">
+                            More
+                        </button>
                     </div>`
                 )
             }
@@ -51,10 +61,6 @@ async function getData() {
         console.log(error);
         alert("sorry could not find that category")
     }
-}
-
-function getCharacterData() {
-
 }
 
 getData();
